2de3e561868a58f64d9a717e0491e9d77b856688
bishal <poudelb891@gmail.com>
2025-02-04 19:07:59 +0545

    base url changed to render

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:7002/api/',
+    baseURL: 'https://know-ur-algo.onrender.com/api/',
     headers: { "Content-Type": "application/json" },
 });
 
